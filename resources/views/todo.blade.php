d727a67962325d86f08ce7478796092c8065f45a
Yandhy Raka <Yandhy Raka>
2017-03-13 11:23:51 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ToDo List</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>ToDo List</h1>
+                <ul>
+                    <li>Taki</li>
+                    <li>Mitsuha</li>
+                    <li>Yotsuha</li>
+                </ul>
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="todo_item" placeholder="Item baru...">
+                    </div>
+                    <button type="submit" class="btn btn-success">Add</button>
+                </form>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
